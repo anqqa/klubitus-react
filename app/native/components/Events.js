939e04705457998741751a78a08e54d8542e4998
anqqa <anttiq@gmail.com>
2017-05-27 18:04:18 +0300

    Use Apollo Client for GraphQL

--- app/native/components/Events.js
+++ app/native/components/Events.js
@@ -1,7 +1,47 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { RefreshControl, ScrollView, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { gql, graphql } from 'react-apollo';
 
+const query = gql`
+  query getLatestEvents {
+    title
+  }
+`;
+
+const EventsWithData = graphql(query, { options: {} })(Events);
+
+
+function Events({ data }) {
+  return (
+    <ScrollView refreshControl={
+      <RefreshControl refreshing={data.networkStatus === 4} onRefresh={data.refetch}/>
+    }>
+      <EventList data={data} />
+    </ScrollView>
+  )
+}
+
+
+function EventList({ data }) {
+  if (data.error) {
+    return <Text>Error! {data.error.message}</Text>
+  }
+
+  if (!data.events) {
+    return <Text>No events found</Text>
+  }
+
+  return (
+    <View>
+      <Text>Events</Text>
+
+      { data.events.map((item) => {
+        return <Text>Event: {item.title}</Text>;
+      })}
+    </View>
+  )
+}
 
 class EventsScreen extends React.Component {
   static navigationOptions = {
@@ -14,11 +54,7 @@ class EventsScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView>
-        <Text>
-          Events
-        </Text>
-      </ScrollView>
+      <EventsWithData/>
     )
   }
 }
--- app/native/containers/index.js
+++ app/native/containers/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import ApolloClient, { createNetworkInterface, ApolloProvider } from 'react-apollo';
 import { TabBarBottom, TabNavigator } from 'react-navigation';
 
 import Routes from '../config/routes';
 
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:8080/graphql'
+});
+
+/*
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {};
+    }
+
+    next();
+  }
+}]);
+*/
+
+const client = new ApolloClient({
+  networkInterface,
+});
 
 const AppNavigator = TabNavigator(Routes, {
   navigationOptions: {
@@ -13,4 +33,7 @@ const AppNavigator = TabNavigator(Routes, {
 });
 
 
-export default() => <AppNavigator />;
+export default() => <ApolloProvider client={client}>
+  <AppNavigator />
+</ApolloProvider>;
+
