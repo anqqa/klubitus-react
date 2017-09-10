741e60ec1f8cddfffa8a01d8bb0095d4b9a1c9af
anqqa <anttiq@gmail.com>
2017-09-10 12:59:36 +0300

    Editor config for IDEs

--- app/src/web/containers/app.js
+++ app/src/web/containers/app.js
@@ -1,6 +1,31 @@
-import React, { Component, PropTypes } from 'react';
+// @flow
 
-class ReactKlubitus extends Component {
+import React, { Component, PropTypes } from 'react';
+import { createNavigator } from 'react-navigation';
+
+
+/**
+ * View with navigation.
+ *
+ * @param  {object}  navigation
+ * @param  {object]  router
+ * @returns  {XML}
+ *
+ * @constructor
+ */
+const NavView = ({ navigation, router }) => {
+  const { state } = navigation;
+  const Component = router.getComponentForState(state);
+
+  return (
+    <Component navigation={addNavigationHelpers({ ...navigation, state: state.routes[state.index] })} />
+  );
+};
+
+
+const App = createNavigator();
+
+class ReactKlubitus extends React.Component {
   render() {
     return (
       <div className="react">
